79b1f8843f59bcbafa458eec3d2bf69d25ff8f96
Nicolas <nicolas@korbit.com>
2019-12-03 21:05:13 -0800

    Testing

---
+++ ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/nico/Desktop/nico/programs/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/nico/Desktop/public_repos/mobile/trace"
+export "FLUTTER_TARGET=/Users/nico/Desktop/public_repos/mobile/trace/lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "FLUTTER_FRAMEWORK_DIR=/Users/nico/Desktop/nico/programs/flutter/bin/cache/artifacts/engine/ios"
